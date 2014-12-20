a98b6e86405730b30a81de4b926f51b41bcd73cb
tigoe <tom.igoe@gmail.com>
2014-12-20 15:24:41 -0500

    Added flicker effect for TreeLights
    This version introduces a candle flicker effect for TreeLights, using
    four closely related colors. The twinkle effect has been temporarily
    removed to get the candle flicker working.

--- BlinkyTree/BlinkyTreeLights/BlinkyTreeLights.ino
+++ BlinkyTree/BlinkyTreeLights/BlinkyTreeLights.ino
@@ -1,9 +1,9 @@
 /*
   BlinkyTree
 
-  This sketch twinkles a pixel white in response to a serial command (x or X).
-  It resets the whole strip to a base color in response to a serial command (z).
-  Continually fades the previously twinkled pixels.
+  This version sets up four colors that are relatively close to each other
+  and fades the pixels randomly between them to create a "candle flicker" effect.
+  The twinkle feature has been removed to get the flicker working.
 
   Uses Adafruit's NeoPixel library: https://github.com/adafruit/Adafruit_NeoPixel
 
@@ -12,14 +12,19 @@
 
 */
 #include <Adafruit_NeoPixel.h>
-#define PIN 13
+#define PIN 13  // the I/O pin that the neopixel data signal is on
 
-const int numPixels = 50;
+const int numPixels = 50;    // number of pixels in the strip
+// initialize the strip:
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(numPixels, PIN, NEO_RGB + NEO_KHZ800);
 
-int change[numPixels];                // whether or not a pixel is changing
-unsigned long pixelColor[numPixels];  // color for each pixel
-unsigned long baseColor = 0x002212;   // In RGB decimal, (0, 34, 18)
+unsigned long targetColor[numPixels];    // next target for each pixel
+unsigned long pixelColor[numPixels];     // current color for each pixel
+
+// range of keyframe colors for the pixels to fade to:
+unsigned long keyColors[] = {0xEB5D19, 0xA4470C, 0x85210C, 0x751E15};
+// count of keyframe colors:
+int numColors = sizeof(keyColors) / 4;
 
 void setup() {
   Serial.begin(9600);     // initialize serial communication
@@ -32,48 +37,53 @@ void loop() {
   // read serial input:
   while (Serial.available() > 0) {
     int input = Serial.read();
+/*
     if (input == 'x' || input == 'X') {    // x signals that you should twinkle a pixel
       int thisPixel = random(numPixels);   // pick a random pixel
       pixelColor[thisPixel] = 0xFFFFFF;    // set its color to white
       change[thisPixel] = 1;               // set its change value to true
     }
+*/
     if (input == 'z') {                    // z signals you should reset the whole strip
       resetStrip();
     }
   }
 
-  // lopp over all the pixels:
-  for (int star = 0; star < numPixels; star++) {
-    // if a pixel's change flag is set:
-    if (change[star] == 1) {
-      // check to see if it matches the base color yet:
-      if (pixelColor[star] == baseColor) {
-        // if it's reached base color again, turn its change flag off
-        change[star] = 0;
-      } else {      // if the pixel hasn't reached base color again,
-        //compare its current color to the base color and fade it closer:
-        unsigned long newColor = compare(pixelColor[star], baseColor);
-        pixelColor[star] = newColor;
-      }
-      strip.setPixelColor(star, pixelColor[star]);// set the color for this pixel
+// iterate over all pixels:
+  for (int thisPixel = 0; thisPixel < numPixels; thisPixel++) {
+    // if the target color matches the current color for this pixel,
+    // then pick a new target color randomly:
+    while (targetColor[thisPixel] == pixelColor[thisPixel]) {
+      int nextColor = random(numColors);
+      targetColor[thisPixel] = keyColors[nextColor];
     }
+    // fade the pixel one step from the current color to the target color:
+    pixelColor[thisPixel] = compare(pixelColor[thisPixel], targetColor[thisPixel]);
+    // set the pixel color in the strip:
+    strip.setPixelColor(thisPixel, pixelColor[thisPixel]);// set the color for this pixel
   }
   // update the strip:
   strip.show();
 }
 
 /*
-  This function resets all the pixels to the base color
-   and turns off all their change flags:
+  This function resets all the pixels to their base colors:
 */
 void resetStrip() {
+  // iterate over the pixels:
   for (int pixel = 0; pixel < numPixels; pixel++) {
-    pixelColor[pixel] = baseColor;
-    change[pixel] = 0;
+    // pick two random colors from the keyframe colors:
+    int nextColor = random(numColors);
+    int lastColor = random(numColors);
+    targetColor[pixel] = keyColors[nextColor];
+    pixelColor[pixel] = keyColors[lastColor];
+    // set the pixel color:
     strip.setPixelColor(pixel, pixelColor[pixel]);// set the color for this pixel
-    strip.show();
   }
+  // update the strip:
+  strip.show();
 }
+
 /*
   takes two colors and returns a color that's a point on each axis (r, g, b)
   closer to the target color
