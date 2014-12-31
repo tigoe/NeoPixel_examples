7707551ee48bb27470dcf1dc52606328fb558599
tigoe <tom.igoe@gmail.com>
2014-12-31 12:15:35 -0500

    Final bug fixes and improvements to BlinkyTree

--- BlinkyTree/BlinkyTreeLights/BlinkyTreeLights.ino
+++ BlinkyTree/BlinkyTreeLights/BlinkyTreeLights.ino
@@ -8,25 +8,27 @@
   * an array to hold only the pixels visible from the front of the tree
   * twinkle lights are picked only from the visible pixel array
   * mailbox support for web interface
-  
-  It adds:
   * mailbox support for fade time and fastForward
-  
+
+  It adds:
+   * various big fixes
 
   Uses Adafruit's NeoPixel library: https://github.com/adafruit/Adafruit_NeoPixel
 
   created 4 Dec 2014
-  updated 26 Dec 2014
+  updated 31 Dec 2014
   by Tom Igoe
 */
 
 #include <Adafruit_NeoPixel.h>
 #include <CapacitiveSensor.h>
 #include <Bridge.h>
-#include <Console.h>
 #include <Mailbox.h>
 
-#define PIN 12  // the I/O pin that the neopixel data signal is on
+const int controlPin = 12;  // the I/O pin that the neopixel data signal is on
+const int numPixels = 50;    // number of pixels in the strip
+// initialize the strip:
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(numPixels, controlPin, NEO_RGB + NEO_KHZ800);
 
 CapacitiveSensor bell = CapacitiveSensor(10, 7);   // initialize cap touch library
 long touchTime = 0;                      // time of last touch
@@ -34,81 +36,69 @@ int touchState = 0;                      // if the bell is being touched
 int threshold = 1000;                    // touch sensitivity threshold
 int touchDelay = 500;                    // how long to delay before resetting touch sensor
 
-const int numPixels = 50;    // number of pixels in the strip
-// initialize the strip:
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(numPixels, PIN, NEO_RGB + NEO_KHZ800);
+// initial reference color range:
+const unsigned long referenceColors[] = {0xCB5D0F, 0xB4470C, 0x95310C, 0x854E0B};
+// the final color of all pixels before they flicker out:
+const unsigned long finalColor = 0x1F0F02;
+// changing range of keyframe colors for the pixels to flicker to:
+unsigned long keyColors[] = {0xCB500F, 0xB4410C, 0x95230C, 0x853E0B};
 
 unsigned long targetColor[numPixels];    // next target for each pixel
 unsigned long pixelColor[numPixels];     // current color for each pixel
-
-// range of keyframe colors for the pixels to flicker to:
-unsigned long keyColors[] = {0xCB500F, 0xB4410C, 0x95230C, 0x853E0B};
-// initial reference color range:
-unsigned long referenceColors[] = {0xCB5D0F, 0xB4470C, 0x95310C, 0x854E0B};
-unsigned long finalColor = 0x1F0F02;
-
 int visiblePixels[] = {6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 40}; // pixels visible on the front of the tree
 int visibleCount = sizeof(visiblePixels) / 2;
 
+
 // count of keyframe colors:
 int numColors = sizeof(keyColors) / 4;
-unsigned long slowFadeInterval =  169400;   // in millis, this makes an 8-hour fade
-unsigned long lastFade = 0;           // timestamp of the last call to fadeToRed()
+unsigned long slowFadeInterval =  169400;   // in millis, the time between fade changes
+unsigned long lastFade = 0;                 // timestamp of the last call to fadeToRed()
+int flickerInterval = 30;                   // in millis, the delay between flicker steps
 
-boolean running = true;    // whether or not the lights are running
-boolean finished = false;
+boolean running = true;    // whether or not your're running
+boolean finished = false;  // whether or not the slow fade is finished
 
 void setup() {
-  Bridge.begin();
-  Mailbox.begin();
-  Console.begin();
-
-  bell.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off touch sensor autocalibrate
+  Bridge.begin();         // initialize Bridge
+  Mailbox.begin();        // initialize Mailbox
   strip.begin();          // initialize pixel strip
-  resetStrip();           // reset the strip
+  resetStrip();           // start the long fade
 }
 
 void loop() {
   String message;
+  // read the next message present in the queue
+  while (Mailbox.messageAvailable()) {
+    Mailbox.readMessage(message);
+    if (message == "off" ) {
+      running = turnOff();
+    }
 
-  if (millis() % 500 < 3) {
-    if (Mailbox.messageAvailable())
-    {
-      // read all the messages present in the queue
-      while (Mailbox.messageAvailable())
-      {
-        Mailbox.readMessage(message);
-        Console.println(message);
-        if (message == "off" ) {
-          running = turnOff();
-        }
+    if (message == "on") {
+      running = resetStrip();
+    }
 
-        if (message == "on") {
-          running = resetStrip();
-        }
+    if (message == "twinkle") {
+      twinkle();
+    }
 
-        if (message == "wings") {
-          twinkle();
-        }
+    if (message.startsWith("fade/")) {
+      String time = message.substring(5);        // get the substring after the /
+      long minutes = time.toFloat();             // convert to a number
+      slowFadeInterval = calcInterval(minutes);  // calculate the interval for slow fade
+    }
 
-        if (message.startsWith("fade/")) {
-          String time = message.substring(5);        // get the substring after the /
-          long minutes = time.toFloat();             // convert to a number
-          slowFadeInterval = calcInterval(minutes);  // calculate the interval for slow fade
-        }
-
-        if (message.startsWith("ff/")) {
-          String completion =  message.substring(3);    // get the substring after the /
-          int completionPercent = completion.toFloat(); // convert to a number
-          fastForward(completionPercent);               // fast forward to the right %
-        }
-      }
+    if (message.startsWith("ff/")) {
+      String completion =  message.substring(3);    // get the substring after the /
+      int completionPercent = completion.toFloat(); // convert to a number
+      fastForward(completionPercent);               // fast forward to the right %
     }
   }
 
-  if (running) {
-    // create the flicker effect every 30ms:
-    if (millis() % 30 < 2) {
+
+  if (running) {      // continue the long fade
+    // create the flicker effect:
+    if (millis() % flickerInterval < 2) {
       flickerPixels();
     }
 
@@ -116,13 +106,9 @@ void loop() {
     if (millis() - lastFade >= slowFadeInterval) {
       finished = fadeToRed();
     }
-    // update the strip:
-    strip.show();
   }
-
-  // if you reached the end of the long fade, turn off:
+  // when you reach the final colors, turn off:
   if (finished) running = turnOff();
-
 
   // read touch sensor:
   long bellTouch =  bell.capacitiveSensor(30);
@@ -138,6 +124,9 @@ void loop() {
   if (millis() - touchTime > touchDelay) {
     touchState = 0;
   }
+
+  // update the strip:
+  strip.show();
 }
 
 /*
@@ -147,7 +136,6 @@ void twinkle() {
   int whichPixel = random(visibleCount);
   int thisPixel = visiblePixels[whichPixel]; // pick a random pixel from the visible list
   pixelColor[thisPixel] = 0xFFDDDD;          // set its color to white
-  Serial1.println("ding");
 }
 
 /*
@@ -189,46 +177,10 @@ boolean fadeToRed() {
 }
 
 /*
-  This function resets all the pixels to their base colors:
-*/
-boolean resetStrip() {
-  // iterate over the pixels:
-  for (int pixel = 0; pixel < numPixels; pixel++) {
-    // pick two random colors from the keyframe colors:
-    int nextColor = random(numColors);
-    int lastColor = random(numColors);
-    targetColor[pixel] = referenceColors[nextColor];
-    pixelColor[pixel] = referenceColors[lastColor];
-    // set the pixel color:
-    strip.setPixelColor(pixel, pixelColor[pixel]);// set the color for this pixel
-  }
-
-  // reset the keyColor array with the reference color array:
-  for (int p = 0; p < numColors; p++) {
-    keyColors[p] = referenceColors[p];
-  }
-
-  return true;
-}
-
-boolean turnOff() {
-  // iterate over the pixels:
-  for (int pixel = 0; pixel < numPixels; pixel++) {
-
-    // set the pixel color:
-    strip.setPixelColor(pixel, 0);// set the color for this pixel
-    strip.show();
-    delay(300);
-  }
-  return false;
-}
-
-/*
   takes two colors and returns a color that's a point on each axis (r, g, b)
   closer to the target color
 */
 unsigned long compare(unsigned long thisColor, unsigned long thatColor) {
-
   // separate the first color:
   byte r = thisColor >> 16;
   byte g = thisColor >>  8;
@@ -254,6 +206,41 @@ unsigned long compare(unsigned long thisColor, unsigned long thatColor) {
 }
 
 /*
+  This function resets all the pixels to their base colors:
+*/
+boolean resetStrip() {
+  // iterate over the pixels:
+  for (int pixel = 0; pixel < numPixels; pixel++) {
+    // pick two random colors from the keyframe colors:
+    int nextColor = random(numColors);
+    int lastColor = random(numColors);
+    targetColor[pixel] = referenceColors[nextColor];
+    pixelColor[pixel] = referenceColors[lastColor];
+    // set the pixel color:
+    strip.setPixelColor(pixel, pixelColor[pixel]);// set the color for this pixel
+  }
+
+  // reset the keyColor array with the reference color array:
+  for (int p = 0; p < numColors; p++) {
+    keyColors[p] = referenceColors[p];
+  }
+  finished = false;
+  return true;
+}
+
+boolean turnOff() {
+  // iterate over the pixels:
+  for (int pixel = 0; pixel < numPixels; pixel++) {
+
+    // set the pixel color:
+    strip.setPixelColor(pixel, 0);// set the color for this pixel
+    strip.show();
+    delay(300);
+  }
+  return false;
+}
+
+/*
   This function calculates the interval for the fadeToRed() steps
 */
 unsigned long calcInterval(long fadeMinutes) {
@@ -270,7 +257,6 @@ unsigned long calcInterval(long fadeMinutes) {
   This function calculates the fadeToRed() steps
 */
 long calcFadeSteps() {
-
   // separate the final color:
   byte targetR = finalColor >> 16;
   byte targetG = finalColor >>  8;
