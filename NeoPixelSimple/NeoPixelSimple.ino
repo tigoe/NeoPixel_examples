9aecbb756138d95e33c1ec05f39db723d1d68890
tigoe <tom.igoe@gmail.com>
2018-06-28 14:11:42 -0400

    updated neoPixelSimple

--- NeoPixelSimple/NeoPixelSimple.ino
+++ NeoPixelSimple/NeoPixelSimple.ino
@@ -6,34 +6,31 @@
   Uses Adafruit's NeoPixel library: https://github.com/adafruit/Adafruit_NeoPixel
 
   created 28 March 2016
+  updated 28 Jun 2018
   by Tom Igoe
 
 */
 #include <Adafruit_NeoPixel.h>
 
 const int neoPixelPin = 5;  // control pin
-const int numPixels = 7;    // number of pixels
+const int pixelCount = 8;    // number of pixels
 
 // set up strip:
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(numPixels, neoPixelPin, NEO_GRBW + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(numPixels, neoPixelPin, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   strip.begin();    // initialize pixel strip
   strip.clear();    // turn all LEDs off
-  strip.show();     // Initialize all pixels to 'off'
 }
 
 void loop() {
-  int red = 0xFF;   // set colors
-  int green = 0x0;
-  int blue = 0x00;
-  int white = 0x10;
+  int red = 255;   // set colors
+  int green = 0;
+  int blue = 0;
 
   // loop over all the pixels:
   for (int pixel = 0; pixel < numPixels; pixel++) {
-    unsigned long color = 0x1200FF00;
-  //  strip.setPixelColor(pixel, red, green, blue, white);// set the color for this pixel
-     strip.setPixelColor(pixel, color);// set the color for this pixel
+    strip.setPixelColor(pixel, red, green, blue);// set the color for this pixel
   }
   strip.show();                                // refresh the strip
 }
