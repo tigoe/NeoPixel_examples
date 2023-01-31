7c92b9ee1f88a7f725bfae714dc1ee987b31e938
Tom Igoe <tom.igoe@gmail.com>
2023-01-31 14:25:01 -0500

    Update NeoPixelRGBWSerialIn.ino

--- NeoPixelRGBWSerialIn/NeoPixelRGBWSerialIn.ino
+++ NeoPixelRGBWSerialIn/NeoPixelRGBWSerialIn.ino
@@ -1,18 +1,20 @@
 /*
   RGBW NeoPixel RGBW control
 
-  This sketch accepts a serial input of picel number, red, green, blue, white\n
+  This sketch accepts a serial input of:
+  C red, green, blue, white\n
+  or H hue, intensity, value\n
 
-  It converts these values to a single RGBW color and sets a neoPixel 
+  It converts these values to a single RGBW color and sets a neoPixel strip
 
   Uses Adafruit's NeoPixel library: https://github.com/adafruit/Adafruit_NeoPixel
 
-  created 30 Jan 2017 
+  created 30 Jan 2017
+  modified 31 Jan 2023
   by Tom Igoe
 
 */
 #include <Adafruit_NeoPixel.h>
-#include "HSI.h"
 
 const int neoPixelPin = 5;  // control pin
 const int pixelCount = 7;    // number of pixels
@@ -30,14 +32,12 @@ void setup() {
 
 void loop() {
   if (Serial.available() > 0) {   // if there's serial data,
-    if (Serial.read() == 'C') {   // and the first byte is C,
-      int pixel = Serial.parseInt();// listen for an ASCII numeric string
-      int r = Serial.parseInt();  // listen for another ASCII numeric string
+    char incoming = Serial.read();
+    if (incoming == 'C') {   // and the first byte is C,
+      int r = Serial.parseInt();  // listen for an ASCII numeric string
       int g = Serial.parseInt();  // listen for another ASCII numeric string
       int b = Serial.parseInt();  // listen for another ASCII numeric string
       int w = Serial.parseInt();  // listen for another ASCII numeric string
-      Serial.print(pixel);
-      Serial.print(",");
       Serial.print(r);            // print what you got
       Serial.print(",");
       Serial.print(g);
@@ -45,8 +45,18 @@ void loop() {
       Serial.print(b);
       Serial.print(",");
       Serial.println(w);
-      strip.setPixelColor(pixel, r, g, b, w); // set the color for this pixel
-      strip.show();   // update the strip
+      for (int p = 0; p < pixelCount; p++) {
+        strip.setPixelColor(p, r, g, b, w); // set the color for this pixel
+      }
+    } else if (incoming == 'H') {
+      unsigned long h = Serial.parseInt();  // listen for an ASCII numeric string
+      int s = Serial.parseInt();  // listen for another ASCII numeric string
+      int v = Serial.parseInt();  // listen for another ASCII numeric string
+      unsigned long color = strip.ColorHSV(h, s, v);
+      for (int p = 0; p < pixelCount; p++) {
+        strip.setPixelColor(p, color);
+      }
     }
+    strip.show();   // update the strip
   }
 }
